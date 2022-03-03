e0b17d153e8ced105d8c22d9fde4cb0c2c0fea73
norikov <norikov151@mail.ru>
2022-03-03 18:25:03 +0500

    Доработка

--- game/Классы визуала/Grass.cs
+++ game/Классы визуала/Grass.cs
@@ -10,22 +10,21 @@ namespace game
 {
     internal class Grass : Sprites
     {
-
         static SolidBrush ForestGreenBrush = new SolidBrush(Color.ForestGreen);
         static SolidBrush GreenBrush = new SolidBrush(Color.Green);
         static SolidBrush DarkBrush = new SolidBrush(Color.DarkGreen);
         static SolidBrush DarkOliveBrush = new SolidBrush(Color.DarkOliveGreen);
 
         static public List<SolidBrush> green_brush_list = new List<SolidBrush>() { ForestGreenBrush, GreenBrush, DarkBrush, DarkOliveBrush };
+        readonly List<SolidBrush> part_color = new List<SolidBrush>();
 
-        List<SolidBrush> part_color = new List<SolidBrush>();
-          
-            public Grass(int in_x, int in_y)
-            {
-                part_color = Sprites.generate_texture(green_brush_list);
-                this.x = in_x; this.y = in_y;
-            }
-        public new void draw_block(PaintEventArgs e)
+        public Grass(int in_x, int in_y)
+        {
+            part_color = Sprites.Generate_texture(green_brush_list);
+            this.x = in_x; this.y = in_y;
+        }
+
+        public void Draw_block(PaintEventArgs e)
         {
 
             Sprites.draw_sprite(x, y, part_color, e);
