157b3c5370108acfc91c697fc9d8319a934791eb
Sneve <cerg.2002@bk.ru>
2022-04-04 20:07:50 +0500

    ctrl + c ctrl + v в building.cs нормального кода

--- game/Классы визуала/Building.cs
+++ game/Классы визуала/Building.cs
@@ -11,32 +11,21 @@ namespace game.Классы_визуала
             global::game.Properties.Resources.Warehouse, global::game.Properties.Resources.Home_Defult,
             global::game.Properties.Resources.Steam_Eng_1lvl
         };
-        int fromWhatChunkX, fromWhatBlockX;
-        int fromWhatChunkY, fromWhatBlockY;
 
         public Building(int X, int Y)
         {
-            x = X ; y = Y;
+            x = X; y = Y;
         }
         public void Draw_building(PaintEventArgs e, Button[] buts, Point DragDelta)
         {
-            int size = new Sprites().GetSpritesSize();
-            //Исправить?
-            fromWhatChunkX = (x - DragDelta.X) / (size * Chunk.ChunkSize);
-            fromWhatChunkY = (y - DragDelta.Y) / (size * Chunk.ChunkSize);
-
-            fromWhatBlockX = ((x - DragDelta.X) % (size * Chunk.ChunkSize)) / size;
-            fromWhatBlockY = ((y - DragDelta.Y) % (size * Chunk.ChunkSize)) / size;
-
-            int xWithDrag = (fromWhatBlockX + fromWhatChunkX * Chunk.ChunkSize) * size + DragDelta.X,
-            yWithDrag = (fromWhatBlockY + fromWhatChunkY * Chunk.ChunkSize) * size + DragDelta.Y;
+            int SpritesSize = new Sprites().GetSpritesSize();
 
             Graphics g = e.Graphics;
-            Rectangle rec = new Rectangle(xWithDrag, yWithDrag, size, size);
+            Rectangle rec = new Rectangle(DragDelta.X % SpritesSize + x, DragDelta.Y % SpritesSize + y, SpritesSize + 1, SpritesSize + 1);
 
             for (int m = 0; m < buts.Length; m++)
             {
-                if(buts[m].FlatAppearance.BorderColor == Color.Blue)
+                if (buts[m].FlatAppearance.BorderColor == Color.Blue)
                 {
                     g.DrawImage(bitmaps[m], rec);
                     g.FillRectangle(new SolidBrush(Color.FromArgb(60, Color.Green)), rec);
